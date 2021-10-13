e5b252132aee647ea14f5469e64da112aa46169e
Derek Roode <droode@nexient.com>
2021-10-13 16:05:45 -0400

    movie row modulized through main component

--- src/components/organisms/MovieRow.js
+++ src/components/organisms/MovieRow.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import YouTube from 'react-youtube'
+import axios from '../.././services/axios'
+import movieTrailer from 'movie-trailer'
 import MovieBlockLarge from "../atoms/MovieBlockLarge";
 import MovieBlockSmall from "../atoms/MovieBlockSmall";
 import SectionTitles from '../atoms/SectionTitles';
@@ -6,18 +9,50 @@ import SectionTitles from '../atoms/SectionTitles';
 import './Organisms.css'
 
 function MovieRow(  
- { movies,
-  movieId,
-  movieImgSrc,
-  movieName,
-  onClickFunction,
+ { 
+  fetchUrl,
   isLargeRow,
   sectionTitle}
   ) {
 
-const handleClick = () =>{
-  console.log("handleClick");
-}
+    const [trailerUrl, setTrailerUrl] = useState('')
+    const [movies, setMovies] = useState([])
+
+    useEffect(()=>{
+      let getData = async () =>{
+        const request = await axios.get(fetchUrl)
+        setMovies(request?.data?.results)
+      }
+      getData()
+  
+    },[fetchUrl])
+
+    const opts = {
+      height: "400",
+      playerVars: {
+        autoplay: 1
+      }
+    }
+
+
+    const handleClick = (movie) =>{
+      if (trailerUrl){
+        setTrailerUrl('')
+      } else {
+        movieTrailer(movie?.title || "")
+        .then((url) =>{
+          console.log("url",url);
+          // https://www.youtube.com/watch?v=XtMThy8QKqU
+          // this will search the url passed in and get the value of v 
+          // in this case it would return  XtMThy8QKqU
+          const urlParams = new URLSearchParams( new URL(url).search)
+          console.log('urlParams',urlParams);
+          setTrailerUrl(urlParams.get('v'))
+        })
+        .catch((error) => console.log("error--->",error))
+      }
+  
+    }
 
   return (
     <div className="row">
@@ -25,10 +60,13 @@ const handleClick = () =>{
 
       <div className="row__images-container">
       {movies.map((movie)=>{
-        console.log("movie",movie);
         return (
           <>
-          {isLargeRow ? (
+            
+          {isLargeRow 
+          
+          ? (
+            
             <MovieBlockLarge 
             movieId={movie.id}
             movieImgSrc={movie.poster_path}
@@ -37,21 +75,27 @@ const handleClick = () =>{
               />
           ) : (
             <MovieBlockSmall 
+            
             movieId={movie.id}
             movieImgSrc={movie.poster_path}
             movieName={movie.name}
             onClickFunction={()=> {handleClick(movie)}}
             />
           )}
-          </>)
+          </>
+          
+          )
         })}
+        
        
         </div>
 
+        <div className="movie__trailers">
 
+{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
 
     </div>
-    
   )
 }
 
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,27 +1,41 @@
 import React from 'react'
 import Banner from '../Banner'
 import NavBar from '../NavBar'
-import Row from '../Row'
 import requests from '../../services/request'
 import "./Main.css"
+import MovieRow from '../organisms/MovieRow'
 
 
 function Main() {
+  const sections = [
+    ["Trending", requests.fetchTrending, "isLargeRow" ],
+    ["Netflix Originals",requests.fetchNetflixOriginals],
+    ["Top Rated",requests.fetchTopRated],
+    ["Action Movies",requests.fetchActionMovies],
+    ["Comedy Movies",requests.fetchComedyMovies],
+    ["Horror Movies",requests.fetchHorrorMovies],
+    ["Romance Movies",requests.fetchRomanceMovies],
+    ["Documentaries",requests.fetchDocumentaries]
+
+  ]
   return (
     <div className="main__container">
+   
       <NavBar/>
       <Banner />
+      {sections.map(([title, endpoint, isLargeRow]) => (
+        <MovieRow
+        sectionTitle={title}
+        fetchUrl={endpoint}
+        isLargeRow={isLargeRow}
+        />
+      ))}
+
 
       
-      <Row sectionTitle={'Trending'} fetchUrl={requests.fetchTrending} isLargeRow/>
-      <Row sectionTitle={'Netflix Originals'} fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row sectionTitle={'Top Rated'} fetchUrl={requests.fetchTopRated}/>
-      <Row sectionTitle={'Action Movies'} fetchUrl={requests.fetchActionMovies}/>
-      <Row sectionTitle={'Comedy Movies'} fetchUrl={requests.fetchComedyMovies}/>
-      <Row sectionTitle={'Horror Movies'} fetchUrl={requests.fetchHorrorMovies}/>
-      <Row sectionTitle={'Romance Movies'} fetchUrl={requests.fetchRomanceMovies}/>
-      <Row sectionTitle={'Documentaries'} fetchUrl={requests.fetchDocumentaries}/>
+
     </div>
+   
   )
 }
 
